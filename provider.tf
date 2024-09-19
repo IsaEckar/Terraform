4bbb2fe11775dcd73c2eec75af69c99a868464bf
IsauraEckar <isauraEckar@gmail.com>
2024-09-18 22:20:22 -0500

    Add provider

---
+++ provider.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+
+ backend "s3" {
+    bucket = "bucket-states1"
+    key = "kubernetes/kubernetes.tstates"
+    encrypt = true
+    region = "us-east-1"
+    shared_credentials_files = ["C:/Users/Isaura/.aws/credentials"]
+    profile = "itm"
+    dynamondb_table = "tstates"
+    }
+}
+
+
+provider "aws" {
+    shared_config_files = ["C:/Users/Isaura/.aws/config"]
+    shared_credentials_files = ["C:/Users/Isaura/.aws/credentials"]
+    profile = "itm"
+    region = "us-east-1"
+}
